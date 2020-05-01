6d74b722d85fd4abf6b07673d1d13f63581dd9c8
mayksuel2727 <mayksuel2727@gmail.com>
2020-05-01 10:03:54 -0300

    Foi remodelada a tela de cadastro de animais e corrigidas algumas fucionalidades da mesma.

--- app/src/main/java/com/example/appnutrigado/view/CadastroAnimais.java
+++ app/src/main/java/com/example/appnutrigado/view/CadastroAnimais.java
@@ -9,9 +9,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -28,10 +30,11 @@ import static com.example.appnutrigado.R.id.spinnerRaca;
 
 
 public class CadastroAnimais extends AppCompatActivity {
-    private String id;
+    private String id, nomeRacas, sexo, montada;
     private EditText editNumBrinco, editNomeAnimais, editDataNasc, editValorAnimal, editPesoAnimal;
     private Button btnCadastra;
     private Spinner racas;
+    private RadioButton masculino, feminino, natural, artificial;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -40,8 +43,19 @@ public class CadastroAnimais extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro__animais);
         inicializarComponentes();
-        inicializarSpinner();
+        Spinner();
         eventoClicks();
+    }
+
+    private void Radio_Button() {
+        if (masculino.isChecked())
+            sexo = "Macho";
+        if (feminino.isChecked())
+            sexo = "femea";
+        if (natural.isChecked())
+            montada = "natural";
+        if (artificial.isChecked())
+            montada = "artificial";
     }
 
     private void eventoClicks() {
@@ -59,13 +73,15 @@ public class CadastroAnimais extends AppCompatActivity {
             public void onClick(View v) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null) {
+                    Radio_Button();
                     String email = user.getEmail();
                     Map<String, Object> animais = new HashMap<>();
                     animais.put("Numero do Brinco", editNumBrinco.getText().toString());
                     animais.put("Nome do Animal", editNomeAnimais.getText().toString());
-
+                    animais.put("Raça", nomeRacas);
+                    animais.put("sexo", sexo);
+                    animais.put("montada", montada);
                     animais.put("Data de Nascimento", editDataNasc.getText().toString());
-                    animais.put("Valor do Animal", editValorAnimal.getText().toString());
                     animais.put("Peso do Animal", editPesoAnimal.getText().toString());
                     System.out.println(id);
                     Log.i("ITALAC", "Incrição estadual: " + id);
@@ -96,13 +112,28 @@ public class CadastroAnimais extends AppCompatActivity {
         editValorAnimal = (EditText) findViewById(R.id.edtValorAnimal);
         editPesoAnimal = (EditText) findViewById(R.id.edtPesoDoAnimal);
         btnCadastra = (Button) findViewById(R.id.btnCadastra);
-        racas = (Spinner)findViewById(spinnerRaca);
-
-
+        racas = (Spinner) findViewById(spinnerRaca);
+        masculino = (RadioButton) findViewById(R.id.masculino);
+        feminino = (RadioButton) findViewById(R.id.femenino);
+        natural = (RadioButton) findViewById(R.id.natural);
+        artificial = (RadioButton) findViewById(R.id.artificial);
     }
 
-    public void inicializarSpinner(){
+    public void Spinner(){
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.racas, android.R.layout.simple_spinner_item);
+        racas.setAdapter(adapter);
+        AdapterView.OnItemSelectedListener escolha = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                nomeRacas = parent.getItemAtPosition(position).toString();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        racas.setOnItemSelectedListener(escolha);
     }
 
     private void alert(String msg) {
